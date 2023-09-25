23d24c19e217d6d5f1f3c1e33b5e56b6cff0783f
SeYn0604 <lkjn111@naver.com>
2023-09-25 19:30:28 +0900

    9.25.(작업자 김세연)
    좌클릭 시 카메라 흔들림 구현
    
    문제점
    1. UI가 화면에 고정되지 않음
    ->ui의 캔버스가 카메라에 종속되어있기에 생기는 문제로 추측

--- 뱀서류/Assets/0.Script/Cam.cs
+++ 뱀서류/Assets/0.Script/Cam.cs
@@ -5,19 +5,52 @@ using UnityEngine;
 public class Cam : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    private float shakeTime;
+    private float shakeLevel;
+
+    private Vector3 shakeOffset = Vector3.zero; // 흔들림 오프셋
+
+    private bool isShaking = false;
+
     void Update()
     {
-        if(target != null)
+        if (target != null)
         {
-            Vector3 vec3 = new Vector3(target.position.x, target.position.y, -10f);
-            transform.position = Vector3.Lerp(vec3, transform.position, Time.deltaTime * 5);
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, -10f);
+            Vector3 desiredPosition = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 5);
+            transform.position = desiredPosition + shakeOffset; // 원래 목표 위치에 흔들림 오프셋을 추가
         }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            OnCameraShake(0.05f, 0.05f);
+        }
+    }
+
+    public void OnCameraShake(float shakeTime = 1.0f, float shakeLevel = 0.01f)
+    {
+        if (isShaking) return;
+
+        this.shakeLevel = shakeLevel;
+        this.shakeTime = shakeTime;
+
+        StartCoroutine(ShakeCamera());
+    }
+
+    private IEnumerator ShakeCamera()
+    {
+        isShaking = true;
+        float endTime = Time.time + shakeTime;
+
+        while (Time.time < endTime)
+        {
+            Vector2 randomOffset = Random.insideUnitCircle * shakeLevel;
+            shakeOffset = new Vector3(randomOffset.x, randomOffset.y, 0); // 흔들림 오프셋을 계산
+
+            yield return null;
+        }
+
+        shakeOffset = Vector3.zero; // 흔들림이 끝나면 오프셋을 0으로 설정
+        isShaking = false;
     }
 }
